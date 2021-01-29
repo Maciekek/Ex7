dadefffdb39077ae9b39fb41851d1cb0da273a6c
Łukasz Kosmiński <lukasz.kosminski@dgt.eu>
2021-01-29 09:08:58 +0100

    initial commit

---
+++ src/main/java/com/isa/bookcase/BookcaseApp.java
@@ -0,0 +1,12 @@
+package com.isa.bookcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcaseApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcaseApp.class, args);
+	}
+}
